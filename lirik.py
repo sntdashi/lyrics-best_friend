005cc2b4497a3a1f82ba6c461efdb4d218225dc7
sntdashi <sntdashi@gmail.com>
2025-10-15 15:34:45 +0000

    update

--- lirik.py
+++ lirik.py
@@ -10,18 +10,18 @@ def jalanin_lirik():
         ("Oh, I still wanna be your favorite boyyyyyyyyyyyyy", 0.093),
         ("I wanna be the one that makes your day", 0.089),
         ("The one you think about as you lie awake", 0.088),
-        ("And I can't wait to be your number,", 0.093),
-        ("your number one", 0.092),
+        ("And I can't wait to be your number,", 0.090),
+        ("your number one", 0.091),
         ("I'll be your biggest fan and you'll be mine", 0.087),
         ("But I still wanna break your heart and make you cry", 0.080)
     ]
 
-    # jeda antar kau dan baris anjg
+    # jeda antar kau dan baris anj
     delay = [
         1.35, 0.8, 0.9, 1.2, 
         1.45, 0.45, 0.25, 
-        0.35,  
-        0.18, 
+        0.18,  
+        0.35, 
         0.35,
         0.4  
     ]
