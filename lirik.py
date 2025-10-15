d096f2c81286c84100950614f99d75a14a615817
sntdashi <sntdashi@gmail.com>
2025-10-15 15:11:08 +0000

    update delays

--- lirik.py
+++ lirik.py
@@ -7,10 +7,11 @@ def jalanin_lirik():
         ("I say that I'm happy", 0.125),
         ("But no, no, no, no", 0.135),
         ("No, no, no", 0.145),
-        ("Oh, I still wanna be your favorite boyyyyyyyyy", 0.093),
+        ("Oh, I still wanna be your favorite boyyyyyyyyyyyyy", 0.093),
         ("I wanna be the one that makes your day", 0.089),
         ("The one you think about as you lie awake", 0.088),
-        ("And I can't wait to be your number, your number one", 0.091),
+        ("And I can't wait to be your number, your number one", 0.093),
+        (" your number one", 0.092)
         ("I'll be your biggest fan and you'll be mine", 0.087),
         ("But I still wanna break your heart and make you cry", 0.080)
     ]
@@ -19,8 +20,9 @@ def jalanin_lirik():
     delay = [
         1.35, 0.8, 0.9, 1.2, 
         1.45, 0.45, 0.25, 
-        0.18,
-        0.35, 
+        0.35,  
+        0.18, 
+        0.35,
         0.4  
     ]
 
