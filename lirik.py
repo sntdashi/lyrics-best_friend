b4c0162ebb7ce306fd3aac079ffa047b5514a383
sntdashi <sntdashi@gmail.com>
2025-10-15 16:38:02 +0000

    update 6

--- lirik.py
+++ lirik.py
@@ -9,21 +9,21 @@ def jalanin_lirik():
         ("No, no, noooooo", 0.145),
         ("Oh, I still wanna be your favorite boyyyyyyyyyyyyy", 0.093),
         ("I wanna be the one that makes your day", 0.089),
-        ("The one you think about as you lie awake", 0.088),
-        ("And I can't wait to be your number,", 0.090),
-        ("your number one", 0.091),
+        ("The one you think about as you lie awakeeee", 0.080),
+        ("And I can't wait to be your number,", 0.091),
+        ("your number oneeeeee", 0.095),
         ("I'll be your biggest fan and you'll be mine", 0.087),
-        ("But I still wanna break your heart and make you cry", 0.080)
+        ("But I still wanna break your heart and make you cry", 0.088)
     ]
 
-    # jeda antar kau dan baris anj
+    # jeda antar baris, disinkronin sama phrasing aslinya
     delay = [
-        1.35, 0.8, 0.9, 1.2, 
-        1.45, 0.45, 0.25, 
-        0.18,  
-        0.35, 
-        0.35,
-        0.4  
+        1.35, 0.6, 0.7, 1.2, 
+        1.06, 0.46, 0.28, 
+        0.22,  # ← dikasih jeda dikit di “number,” sebelum lanjut ke baris berikutnya
+        0.36, 
+        0.33,
+        0.45  
     ]
 
     print("\n== Favorite Boy - Rex Orange County ==")
@@ -34,6 +34,6 @@ def jalanin_lirik():
         print('')
         if i < len(delay):
             time.sleep(delay[i])
-    print("\n// Code by rawzn <3")
+    print("\n// Code by rawrzn <3")
 
 jalanin_lirik()
